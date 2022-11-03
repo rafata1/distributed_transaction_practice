2baa6e8a5dc0a07bf6ddc3c2ba84b3d7fa8b2c17
rafata1 <caodung566@gmail.com>
2022-11-03 10:55:08 +0700

    Add files via upload
    update test

--- transactions.go
+++ transactions.go
@@ -1,10 +1,3 @@
-// go-bigtx - Simple two phase commits implementation on MongoDB with Golang
-
-// Copyright (c) 2016 Chaiwat Shuetrakoonpaiboon. All rights reserved.
-//
-// Use of this source code is governed by a MIT license that can be found in
-// the LICENSE file.
-
 package bigtx
 
 import (
